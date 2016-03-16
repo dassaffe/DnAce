9d6ee4beebd46b016aacfd8f4a30816eeaefb4b0
cKarsten <wishmop@online.de>
2016-03-16 21:18:14 +0100

    Corrected namespace

--- Engine/Game1.cs
+++ Engine/Game1.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace DnAce
+namespace Engine
 {
     /// <summary>
     /// This is the main type for your game.
